a3acc34a14249642277f0fe23a1a609e19cc86ad
dustinii <125335987+dustinii@users.noreply.github.com>
2023-09-11 14:24:03 -0700

    api routes finished

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,38 +6,38 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-  Tag.findAll({})
-    .then()
-    .catch();
+  Tag.findAll({ include: [{ model: Product, through: ProductTag }] })
+    .then((tags) => res.status(200).json(tags))
+    .catch((err) => res.status(500).json(err));
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-  Tag.findOne()
-    .then()
-    .catch()
+  Tag.findOne({ where: { id: req.params.id }, include: [{ model: Product, through: ProductTag }] })
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err));
 });
 
 router.post('/', (req, res) => {
   // create a new tag
-  Tag.create()
-    .then()
-    .catch();
+  Tag.create(req.body)
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err));
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
-  Tag.update()
-    .then()
-    .catch()
+  Tag.update(req.body, { where: { id: req.params.id } })
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err));
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
-  Tag.destroy()
-    .then()
-    .catch()
+  Tag.destroy({ where: { id: req.params.id } })
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err));
 });
 
 module.exports = router;
