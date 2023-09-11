06a86f9edb8ac49f3eebbac61cc02634508dbe58
dustinii <125335987+dustinii@users.noreply.github.com>
2023-09-11 08:33:47 -0700

    product routes templated

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,18 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({})
+    .then()
+    .catch();
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({})
+    .then()
+    .catch();
 });
 
 // create new product
@@ -91,6 +97,9 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({})
+    .then()
+    .catch();
 });
 
 module.exports = router;
