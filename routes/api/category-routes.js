937ba006cef39e4629fad102e52efbb1b482f739
dustinii <125335987+dustinii@users.noreply.github.com>
2023-09-11 08:31:17 -0700

    category routes templated

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,38 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({})
+    .then()
+    .catch();
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({})
+    .then()
+    .catch();
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create()
+    .then()
+    .catch();
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({})
+    .then()
+    .catch();
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({})
+    .then()
+    .catch();
 });
 
 module.exports = router;
